7419360c4e73ae7882c2fc049329184da737e11b
Hirumi Weerasooriya <155351049+hirumiP@users.noreply.github.com>
2025-04-02 11:02:31 +0530

    modified icons and namings

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,35 +1,31 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import {
   FIREBASE_API_KEY,
   FIREBASE_AUTH_DOMAIN,
   FIREBASE_PROJECT_ID,
   FIREBASE_STORAGE_BUCKET,
   FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_APP_ID
-} from './config';
+  FIREBASE_APP_ID,
+} from "./config";
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
   authDomain: FIREBASE_AUTH_DOMAIN,
   projectId: FIREBASE_PROJECT_ID,
   storageBucket: FIREBASE_STORAGE_BUCKET,
   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID
+  appId: FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const FIREBASE_APP = initializeApp(firebaseConfig);
-const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
-});
+const FIREBASE_AUTH = initializeAuth(FIREBASE_APP);  // 🔥 REMOVE PERSISTENCE TEMPORARILY
 const FIREBASE_DB = getFirestore(FIREBASE_APP);
-
 
 export { FIREBASE_APP, FIREBASE_AUTH, FIREBASE_DB };
 
-console.log ("Firebase connected Successfully");
+console.log("Firebase connected Successfully");
